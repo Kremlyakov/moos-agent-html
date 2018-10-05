47246601f7c414f02dce3dff9b676a50fa5676c4
Artem Serdezhenko <timaxphp@gmail.com>
2018-10-05 13:15:03 +0300

    0005

--- js/core.js
+++ js/core.js
@@ -2,8 +2,22 @@ $(function(){
 
   console.log('page loaded!');
 
+  initFrUserMenu();
+
   $('#rangeSlider').slider({
 
   });
 
+
+  function initFrUserMenu(){
+    let userMenuBtn = $('.fr-user-menu__main');
+
+    if(userMenuBtn.length === 0) return false;
+
+    userMenuBtn.on('click', function(e){
+      e.preventDefault();
+
+      userMenuBtn.parent().toggleClass('open');
+    });
+  }
 });
